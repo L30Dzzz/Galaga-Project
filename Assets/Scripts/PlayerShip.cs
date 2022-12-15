d133e9e89fd1fa0d025ce8b35bb5dafa8bcfb618
L30Dzzz <113184803+L30Dzzz@users.noreply.github.com>
2022-12-15 09:10:18 -0500

    set up

--- Assets/Scripts/PlayerShip.cs
+++ Assets/Scripts/PlayerShip.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class PlayerShip : MonoBehaviour
 {
+    public float speed = 3.0f;
+    
+    public int maxHealth = 3;
+    
+    public GameObject projectilePrefab;
+    
+    public int health { get { return currentHealth; }}
+    int currentHealth;
+    
+    public float timeInvincible = 2.0f;
+    bool isInvincible;
+    float invincibleTimer;
+    
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Write the code below to get your Rigidbody2D component
+        rigidbody2d = GetComponent<Rigidbody2D>();
+
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Write the code below to get your Horizontal and Vertical axis
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+    }
+
+    void FixedUpdate()
+    {   
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
         
+        rigidbody2d.MovePosition(position);
     }
 }
